861c4809579c4f5e90dd71bbeb7c0b470843db9d
ichihara-3 <k.ichihara.1@gmail.com>
2017-05-27 21:26:57 +0900

    enable to pass hash as parameters

--- ruby/tree.rb
+++ ruby/tree.rb
@@ -4,10 +4,23 @@
 class Tree
   attr_accessor :children, :node_name
 
-  def initialize(name, children=[])
-      @children = children
-      @node_name = name
+  def initialize(tree)
+      tree.each do |parent, children|
+        @children = self.children_node(children) 
+        @node_name = parent
+      end
   end
+
+  def children_node(children)
+    c = []
+    unless children.is_a?(Array)
+      children.each do |key, value|
+        c.push(Tree.new({key => value}))
+      end
+    end
+    c
+  end
+
 
   def visit_all(&block)
     visit &block
@@ -19,9 +32,11 @@ class Tree
   end
 end
 
-ruby_tree = Tree.new( "Ruby",
-  [Tree.new("Reia"),
-   Tree.new("MacRuby")] )
+ruby_tree = Tree.new({
+    'grandpa' => { 
+        'dad' => {
+            'child 1' => [],
+            'child 2' => []}}})
 
 puts "Visiting a node"
 ruby_tree.visit {|node| puts node.node_name}
