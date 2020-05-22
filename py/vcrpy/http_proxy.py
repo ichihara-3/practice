c6a59d97f8a1b6c09c1fe2fc95371b4492e16e6f
ichi_taro3 <taro3.ichi@gmail.com>
2020-05-22 23:01:25 +0900

    to know how to get raw body data, return body data

--- py/vcrpy/http_proxy.py
+++ py/vcrpy/http_proxy.py
@@ -1,18 +1,19 @@
-from bottle import Bottle, run, request, get, post
+from bottle import Bottle, run, request
 import requests
 
 
 app = Bottle()
 
 
-@app.route("/")
+@app.route("/", method="ANY")
 def proxy():
     lines = []
     for k, v in request.headers.items():
-        lines.append(f'{k}={v}')
-    return '<br>'.join(lines)
+        lines.append(f"{k}={v}")
+    body = "<br>".join(lines)
 
-
+    body += request.body.read().decode()
+    return body
 
 
 def main():
