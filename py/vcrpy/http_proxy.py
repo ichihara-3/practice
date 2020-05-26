5f0ecbc12b31f828e4c723285a4c2db93b3e7a2d
ichi_taro3 <taro3.ichi@gmail.com>
2020-05-26 23:07:59 +0900

    enable to set port number

--- py/vcrpy/http_proxy.py
+++ py/vcrpy/http_proxy.py
@@ -1,15 +1,19 @@
 import wsgiref
 
 from bottle import Bottle, run, request, HTTPResponse
+import argparse
+import bottle
 import requests
 import vcr
-import bottle
 
 bottle.debug(True)
 
 
 class ProxyBottle(Bottle):
     def __call__(self, environ, start_response):
+        """Wrapping start_response to filter headers,
+        because hop-by-hop headers are not allowed in wsgi implementation.
+        """
         def wrapped_start_response(status, headerlist, exc_info=None):
             headerlist = [
                 (key, value)
@@ -45,8 +49,18 @@ def proxy(url):
 
 
 def main():
-    port = 8000
+    args = get_arguments()
+    port = args.port
+
     run(app, port=port)
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', '-p', default=8000, type=int, help='port number to serve')
+
+    return parser.parse_args()
+
 
 
 if __name__ == "__main__":
