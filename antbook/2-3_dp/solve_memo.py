6ae13fd8a8340ce9b8c333f2bd2bac1dab79b377
ichi_taro3 <taro3.ichi@gmail.com>
2020-07-31 08:37:30 +0900

    メモ化再帰で高速化

---
+++ antbook/2-3_dp/solve_memo.py
@@ -0,0 +1,39 @@
+# 重さ w(i) 価値 v(i) のn個の品物
+# 重さの総和がWを超えないように選んだ時の、max(∑v)
+
+dp = [[-1 for i in range(101)] for j in range(101)]
+
+def main():
+    ans = 0
+    n = int(input())
+    wv = []
+    for i in range(n):
+        wv.append(list(map(int, input().split())))
+    w = int(input())
+
+    ans = rec(0, w, wv)
+
+    answer=int(input())
+    print('result:', ans)
+    print('answer:', answer)
+    assert answer == ans
+
+
+def rec(i, w, wv):
+    if dp[i][w] >= 0:
+        return dp[i][w]
+    result = 0
+    # 全ての品物を検査済
+    if i == len(wv):
+        result = 0
+    elif w < wv[i][0]:
+        result = rec(i+1, w, wv)
+    else:
+        result = max(rec(i+1, w, wv), rec(i+1, w-wv[i][0], wv) + wv[i][1])
+    dp[i][w] = result
+    return result
+
+
+
+if __name__ == '__main__':
+    main()
