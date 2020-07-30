cdfb9b7b706db96abfaafd168d612b7debb8b3ad
ichi_taro3 <taro3.ichi@gmail.com>
2020-07-31 08:43:56 +0900

    コメント追加、手動テストケース追加

--- antbook/2-3_dp/solve_memo.py
+++ antbook/2-3_dp/solve_memo.py
@@ -1,6 +1,8 @@
 # 重さ w(i) 価値 v(i) のn個の品物
 # 重さの総和がWを超えないように選んだ時の、max(∑v)
 
+# メモ
+# dp[i][w] i番目以降で最大値がwのときの価値
 dp = [[-1 for i in range(101)] for j in range(101)]
 
 def main():
@@ -26,9 +28,12 @@ def rec(i, w, wv):
     # 全ての品物を検査済
     if i == len(wv):
         result = 0
+
+    # 重さが上限を超えているなら採用せず次の品物
     elif w < wv[i][0]:
         result = rec(i+1, w, wv)
     else:
+        # max(この品物を採用しないパターン vs 採用するパターン)
         result = max(rec(i+1, w, wv), rec(i+1, w-wv[i][0], wv) + wv[i][1])
     dp[i][w] = result
     return result
