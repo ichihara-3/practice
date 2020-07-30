d754ef2a948552fd31be03a35b5db72f26714ca3
ichi_taro3 <taro3.ichi@gmail.com>
2020-07-31 08:36:59 +0900

    テストケース再生成

--- antbook/2-3_dp/run.sh
+++ antbook/2-3_dp/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
+script=${1:-solve_fullsearch.py}
 for f in $(ls ./testcases/*.txt |sort -t t -k 2 -n) ; do
-  echo ${f}; cat ${f} | time python3 solve_fullsearch.py
+  echo ${f}; cat ${f} | time python3 ${script}
   echo --------
 done
