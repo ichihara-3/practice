99c86d17c2eee82b3fdc0cfbf2ac14cc0f42a9df
ichi_taro3 <taro3.ichi@gmail.com>
2020-08-03 23:28:09 +0900

    LCS問題のdp解法を追加

--- antbook/2-3_dp/LCS/solve_dp.py
+++ antbook/2-3_dp/LCS/solve_dp.py
@@ -1,40 +1,37 @@
 # LCS: Longest common subsequence
-# dp[i][j]: Sのi番目以降, Tのj番目以降での最長共通文字列
-# dp[i][j] = 0 (i==len(S)-1 or j == len(T)-1)
-# dp[i][j] = max(dp[i+1][j], dp[i][j+1]) (S[i] != T[i])
+# dp[i][j]: Sのi-1番目まで, Tのj-1番目まででの最長共通文字列
+# dp[0][0] = 0 長さ0の文字列
+# dp[0][1] = 0 長さ1の文字列と長さ0の文字列
+# dp[1][0] = 0 長さ0の文字列と長さ1の文字列
+# dp[i][j] = max(dp[i-1][j], dp[i][j-1]) (S[i] != T[j])
 # dp[i][j] = max(
-#                dp[i+1][j+1]+1  (共通文字列を利用する),
-#                dp[i+1][j]      (共通文字列を利用しない),
-#                dp[i][j]        (共通文字列を利用しない),
+#                dp[i-1][j-1]+1  (共通文字列を利用する),
+#                dp[i-1][j]        (共通文字列を利用しない),
+#                dp[i][j-1]        (共通文字列を利用しない),
 #            )
 
 
-memo = [[-1 for _ in range(1001)] for _ in range(1001)]
+dp = [[0 for _ in range(1001)] for _ in range(1001)]
 
 def main():
     S = input()
     T = input()
 
-    result = solve(0, 0, S, T)
+    result = solve(S, T)
     print(result)
 
     ans = int(input())
     assert result == ans, f'answer: {ans:d}, result: {result:d}'
 
 
-def solve(i, j, S, T):
-    result = 0
-    if memo[i][j] >= 0:
-        return memo[i][j]
-    if len(S[i:]) == 0 or len(T[j:]) == 0:
-        result = 0
-    elif S[i] != T[j]:
-        result = max(solve(i+1, j, S, T), solve(i, j+1, S, T))
-    else:
-        result = max(solve(i+1, j+1, S, T) + 1, solve(i+1, j, S, T), solve(i, j+1, S, T))
-    memo[i][j] = result
-    return result
-
+def solve(S, T):
+    for i, s in enumerate(S, start=1):
+        for j, t in enumerate(T, start=1):
+            if s != t:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            else:
+                dp[i][j] = max(dp[i-1][j-1] + 1, dp[i-1][j], dp[i][j-1])
+    return dp[len(S)][len(T)]
 
 
 if __name__ == "__main__":
