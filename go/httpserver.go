4e4b4e34d27f8e5fd29e323c8430019bd2a023d1
ichi_taro3 <taro3.ichi@gmail.com>
2020-09-06 11:30:26 +0900

    httpserver.goをちょっと試してみてる

--- go/httpserver.go
+++ go/httpserver.go
@@ -4,18 +4,40 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "requested\n")
+	defer log(w, r)
+	fmt.Fprintln(w, "hello")
+}
+
+type CountHandler struct {
+	mu sync.Mutex
+	n  int
+}
+
+func (c *CountHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer log(w, r)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.n++
+	fmt.Fprintf(w, "count: %v\n", c.n)
+}
+
+func log(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	uri := r.RequestURI
 	var headers []string
 	for k, v := range r.Header {
 		headers = append(headers, fmt.Sprintf("%v: %v", k, v))
 	}
-	fmt.Fprintf(w, strings.Join(headers, "\n"))
+	fmt.Printf("%v\t%s\tHeaders: %s\n", now, uri, strings.Join(headers, ","))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.Handle("/count", new(CountHandler))
 	http.ListenAndServe(":8080", nil)
 }
