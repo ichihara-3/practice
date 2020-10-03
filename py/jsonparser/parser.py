002843fa842864d1fc1ab58656739d89b6c089f8
ichi_taro3 <taro3.ichi@gmail.com>
2020-10-03 12:48:59 +0900

    jsonparser: arrayを拡張、ws textを扱えるようにする

--- py/jsonparser/parser.py
+++ py/jsonparser/parser.py
@@ -1,10 +1,18 @@
+class WS:
+
+    values = ('\x20', '\x09', '\x0a', '\x0d')
 
 
 class Parser:
 
     def parse(self, string):
+        string = self._trim_ws(string)
         if string == 'null':
             return None
+        if string == 'true':
+            return True
+        if string == 'false':
+            return False
         if string[0] == '[':
             contents = []
             element = ''
@@ -20,7 +28,17 @@ class Parser:
                 element += s
             return list(map(self.parse, contents))
         if string[0] == '"':
-            return str(string[1:-1])
+            line = ''
+            for s in string[1:]:
+                if s == '"':
+                    break
+                line += s
+            return str(line)
         if '.' in string:
             return float(string)
         return int(string)
+
+
+    def _trim_ws(self, string):
+        return string.strip(''.join(WS.values))
+
--- py/jsonparser/test_parser.py
+++ py/jsonparser/test_parser.py
@@ -11,9 +11,29 @@ def test_int():
     assert result == expected
 
 
+def test_int_surrounded_by_ws():
+    p = parser.Parser()
+
+    string = '\t\r\n 1 \t\r\n '
+    expected = 1
+
+    result = p.parse(string)
+    assert result == expected
+
+
 def test_float():
     p = parser.Parser()
     string = '1.5'
+    expected = 1.5
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
+def test_float_surrounded_by_ws():
+    p = parser.Parser()
+    string = ' \t\r\n1.5 \t\r\n     '
     expected = 1.5
 
     result = p.parse(string)
@@ -29,6 +49,18 @@ def test_str():
     result = p.parse(string)
 
     assert result == expected
+
+
+def test_str_surrounded_by_ws():
+    p = parser.Parser()
+    string = ' \t\r\n "string" \t\r\n     '
+    expected = "string"
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
 
 def test_empty_str():
     p = parser.Parser()
@@ -48,6 +80,58 @@ def test_null():
     result = p.parse(string)
 
     assert result == expected
+
+
+def test_null_surrounded_by_ws():
+    p = parser.Parser()
+    string = ' \t\r\n null \t\r\n '
+    expected = None
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
+def test_true():
+    p = parser.Parser()
+    string = 'true'
+    expected = True
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
+def test_true_surrounded_by_ws():
+    p = parser.Parser()
+    string = ' \t\r\n true \t\r\n '
+    expected = True
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
+def test_false():
+    p = parser.Parser()
+    string = 'false'
+    expected = False
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
+def test_false_surrounded_by_ws():
+    p = parser.Parser()
+    string = ' \t\r\n false \t\r\n '
+    expected = False
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
 
 
 def test_empty_array():
@@ -70,3 +154,31 @@ def test_int_in_array():
 
     assert result == expected
 
+
+def test_str_in_array():
+    p = parser.Parser()
+    string = '["a","helloworldxxx"]'
+    expected = ["a","helloworldxxx"]
+
+    result = p.parse(string)
+
+    assert result == expected
+
+
+def test_values_in_array():
+    p = parser.Parser()
+    string = '[null,true,false]'
+    expected = [None, True, False]
+
+    result = p.parse(string)
+
+    assert result == expected
+
+def test_array_with_ws():
+    p = parser.Parser()
+    string = '\n  \t \r [ \n\r\t null \r \t \n ,\r\n\t true \r\n\t ,        false \t\t\t\t\t\t\r\n]    \r\n'
+    expected = [None, True, False]
+
+    result = p.parse(string)
+
+    assert result == expected
