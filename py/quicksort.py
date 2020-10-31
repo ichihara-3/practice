3c6bbae02249b445a3b2ef288a5d0de269c70db3
ichi_taro3 <taro3.ichi@gmail.com>
2020-10-31 22:58:46 +0900

    quicksortを実装

---
+++ py/quicksort.py
@@ -0,0 +1,58 @@
+import argparse
+import sys
+
+sys.setrecursionlimit(100000)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+
+    args = parser.parse_args()
+
+    data = []
+    with open(args.filename) as f:
+        for line in f:
+            try:
+                data.extend(list(map(int, line.split())))
+            except ValueError as e:
+                print('入力データが不正です')
+                print('-------- error情報 --------')
+                print(e.with_traceback(sys.exc_info()[2]))
+                sys.exit(1)
+
+    quicksort(data)
+    for i in range(0, len(data), 10):
+        print(' '.join(map(str, data[i:i+10])))
+
+
+def quicksort(data):
+    _quicksort(data, 0, len(data)-1)
+
+
+def _quicksort(data, left, right):
+    i = left
+    j = right
+    if i >= j:
+        return
+    pivot = data[i+(j-i)//2]
+    while i <= j:
+        while data[i] < pivot:
+            i += 1
+        while data[j] > pivot:
+            j -= 1
+        if i >= j:
+            break
+        swap(data, i, j)
+        i += 1
+        j -= 1
+    _quicksort(data, left, i-1)
+    _quicksort(data, j+1, right)
+
+
+def swap(data, x, y):
+    data[x], data[y] = data[y], data[x]
+
+
+
+if __name__ == '__main__':
+    main()
