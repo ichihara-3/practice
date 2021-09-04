a3c31c7863d15f4e5c18f03345778d441ba27fce
ichi_taro3 <taro3.ichi@gmail.com>
2021-09-04 11:05:43 +0900

    compile error for moved var

--- rust/hello-rust/src/main.rs
+++ rust/hello-rust/src/main.rs
@@ -1,4 +1,13 @@
+// fn main() {
+//     let a = String::from("Hello");
+//     println!("{}, world!", a);
+// }
+
+// compile error
 fn main() {
     let a = String::from("Hello");
+    let b = a;
+    println!("{}, world!", b);
     println!("{}, world!", a);
+
 }
