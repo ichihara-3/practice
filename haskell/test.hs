7f52a3fde55d1b522c0799dd7f544d04cf66345e
ichi_taro3 <taro3.ichi@gmail.com>
2022-12-24 20:28:00 +0900

    hello haskell

---
+++ haskell/test.hs
@@ -0,0 +1,22 @@
+double x = x + x
+quadruple x = double (double x)
+factorial n = product [1..n]
+average ns = sum ns `div` length ns
+
+a = b + c
+  where
+    b = 5
+    c = 10
+
+d = a * 2
+
+z = double a
+
+n = a `div` length xs
+  where
+    a = 10
+    xs = [1,2,3,4,5]
+
+mylast xs = head (reverse xs)
+mylast2 xs = xs !! ((length xs)-1)
+mylast3 xs = head (drop ((length xs) -1) xs)
