9963550913f2f921c5ba8dc2436b0a8bd4e807ad
ichi_taro3 <taro3.ichi@gmail.com>
2023-02-27 00:06:37 +0900

    higher order function

--- haskell/higherorder_function.hs
+++ haskell/higherorder_function.hs
@@ -1,3 +1,6 @@
+import Data.Char
+import Data.List
+
 twice :: (a -> a) -> a -> a
 twice f x = f (f x)
 
@@ -73,3 +76,80 @@ twice2 f = f . f
 
 compose :: [a -> a] -> (a -> a)
 compose = foldr (.) id
+
+---------
+-- binary converter
+
+
+type Bit = Int
+
+bin2int :: [Bit] -> Int
+bin2int bits = sum [w * b | (w, b) <- zip weights bits]
+               where weights = iterate (*2) 1
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+encode :: String -> [Bit]
+encode = concat . map (make8 . int2bin . ord)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 [] = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int) . chop8
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+channel = id
+
+--------
+-- vote algorithms
+
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x: rmdups (filter (/= x) xs)
+
+result :: Ord a => [a] -> [(Int, a)]
+result xs = sort [(count v xs, v) | v <- rmdups xs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+
+--
+ballots :: [[String]]
+ballots = [["Red", "Green"],
+           ["Blue"],
+           ["Green", "Red", "Blue"],
+           ["Blue", "Green", "Red"],
+           ["Green"]]
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= [])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+             [c]    -> c
+             (c:cs) -> winner' (elim c bs)
