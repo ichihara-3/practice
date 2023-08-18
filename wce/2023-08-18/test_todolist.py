056252b292e2339f25b7f1237c6df492ecdb20d9
ichi_taro3 <taro3.ichi@gmail.com>
2023-08-19 00:20:32 +0900

    Write Code Everyday day6: TDD with TodoList
    (* it's 0AM now, so actually this is day7.)

--- wce/2023-08-18/test_todolist.py
+++ wce/2023-08-18/test_todolist.py
@@ -5,13 +5,42 @@ from todolist import TodoList
 
 class TestTodoList:
 
-    def test_todo_add(self):
-        _todo = TodoList()
-        _todo.add('test')
-        assert _todo.size == 1
+    def test_push(self):
+        todo = TodoList()
+        todo.push('test')
+        assert todo.size == 1
+        todos = todo.list_todo()
+        assert len(todos) == 1
+        assert todos[0] == 'test'
 
-    def test_todo_add_multiple(self):
-        _todo = TodoList()
-        _todo.add('test')
-        _todo.add('test2')
-        assert _todo.size == 2
+    def test_push_multiple(self):
+        todo = TodoList()
+        todo.push('test')
+        todo.push('test2')
+        assert todo.size == 2
+        todos = todo.list_todo()
+        assert len(todos) == 2
+        assert todos[0] == 'test'
+
+    def test_pop(self):
+        todo = TodoList()
+        todo.push('test')
+        assert todo.pop(0) == 'test'
+
+    def test_make_done(self):
+        todo = TodoList()
+        todo.push('test')
+        todo.make_done(0)
+        assert todo.is_done(0)
+
+    def test_list_done(self):
+        todo = TodoList()
+        todo.push('test')
+        todo.make_done(0)
+        assert todo.list_done() == ['test']
+
+    def test_list_todo(self):
+        todo = TodoList()
+        todo.push('test')
+        todo.make_done(0)
+        assert todo.list_todo() == []
--- wce/2023-08-18/todolist.py
+++ wce/2023-08-18/todolist.py
@@ -3,9 +3,30 @@ class TodoList:
 
     def __init__(self):
         self._size = 0
+        self._items = []
+        self._dones = []
 
-    def add(self, item):
+    def push(self, item: str):
         self._size += 1
+        self._items.append(item)
+        self._dones.append(False)
+
+    def pop(self, index: int) -> str:
+        self._size -= 1
+        self._dones.pop(index)
+        return self._items.pop(index)
+
+    def list_todo(self) -> list:
+        return [self._items[i] for i in range(self._size) if not self._dones[i]]
+
+    def make_done(self, index: int):
+        self._dones[index] = True
+
+    def is_done(self, index: int) -> bool:
+        return self._dones[index]
+
+    def list_done(self) -> list:
+        return [self._items[i] for i in range(self._size) if self._dones[i]]
 
     @property
     def size(self):
